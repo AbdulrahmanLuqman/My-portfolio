d11814f538b7e41d1ef72df803edca5f046eef09
AbdulrahmanLuqman <fal528@dervac.com>
2023-08-10 06:16:05 +0100

    first commit

---
+++ src/components/allSection/SixthSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="text-[whitesmoke] space-y-10">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-delay="100" data-aos-easing="ease-in-out" class="border px-4 py-2 rounded-[30px] flex items-center gap-4 w-fit">
+            <svg class="w-full" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5a1 1 0 1 0 2 0a1 1 0 1 0-2 0m0 7a1 1 0 1 0 2 0a1 1 0 1 0-2 0m0 7a1 1 0 1 0 2 0a1 1 0 1 0-2 0m6-14a1 1 0 1 0 2 0a1 1 0 1 0-2 0m0 7a1 1 0 1 0 2 0a1 1 0 1 0-2 0m0 7a1 1 0 1 0 2 0a1 1 0 1 0-2 0"/></svg>
+            <p class="uppercase text-sm">Portfolio</p>
+        </div>
+        <p data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-delay="100" data-aos-easing="ease-in-out" class="md:text-6xl text-4xl">Featured <span class="text-[#F31313]">Projects</span></p>
+        
+        <div class="grid grid-cols-2">
+            
+        </div>
+    </section>
+</template>
--- src/views/views.vue
+++ src/views/views.vue
@@ -1,19 +1,42 @@
 <script>
 import { defineComponent } from 'vue'
+// import { eventBus } from '../eventbus.js'
 import FirstSection from '../components/allSection/FirstSection.vue';
+import SecondSection from '../components/allSection/SecondSection.vue'
+import ThirdSection from '../components/allSection/ThirdSection.vue'
+import FourthSection from '../components/allSection/FourthSection.vue';
+import FifthSection from '../components/allSection/FifthSection.vue'
+import SixthSection from '../components/allSection/SixthSection.vue';
+
 
 export default defineComponent({
-    components:{FirstSection},
+    components:{ FirstSection, SecondSection, ThirdSection, FourthSection, FifthSection, SixthSection },
     data(){
         return{
 
         }
-    }
+    },
+    methods: {
+    handleScrollToSection(sectionId) {
+      const sectionElement = document.getElementById(sectionId);
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    },
+  },
+
 })
 </script>
 
 <template>
-   <FirstSection />
+   <div class="lg:pl-12 lg:px-6 px-2 py-20 w-full">
+    <FirstSection class="py-2" />
+    <SecondSection class="py-[160px]" />
+    <ThirdSection class="py-[80px]" />
+    <FourthSection class="py-[80px]" />
+    <FifthSection class="py-[80px]" />
+    <SixthSection class="py-[80px]" />
+   </div>
 </template>
 
 <style scoped>
